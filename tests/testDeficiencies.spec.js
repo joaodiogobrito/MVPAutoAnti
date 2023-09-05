9b3e72222b85541f0f13c5ba8b9feacc80d0f74d
Joao Brito <joaodiogobrito@gmail.com>
2023-09-05 13:07:24 +0100

    Amend testDeficiencies

--- tests/testDeficiencies.spec.js
+++ tests/testDeficiencies.spec.js
@@ -27,23 +27,22 @@ test('test', async ({ page }) => {
 
     //Test details page
     await page.getByRole('cell', { name: 'Innenbereich' }).click();
-    await expect.soft(page.getByText('Aktualisierung von Mängeln')).toBeVisible();
+    await expect.soft(page.getByText('Update deficiencies')).toBeVisible();
     await expect.soft(page.getByText('offen stehendes Lagertor')).toBeVisible();
 
     //Test Update Deficiency textbox
     await page.getByRole('textbox').click();
     await page.getByRole('textbox').fill('test123');
+    await page.getByLabel('Update deficiencies').locator('span').first().click();
     await page.getByRole('button', { name: 'Update Deficiency' }).click();
-    await page.getByRole('cell', { name: 'Innenbereich' }).click();
     await expect.soft(page.getByText('test123')).toHaveText('test123');
     
     //Test Close a deficiency
-    await page.getByLabel('Aktualisierung von Mängeln').locator('span').first().click();
+    await page.locator('div').filter({ hasText: /^OpenSelect Category$/ }).getByLabel('Clear selected options').click(); 
+    await page.getByRole('cell', { name: 'Innenbereich' }).click();
+    await page.getByText('test123').fill('');
+    await page.locator('label div').click();
     await page.getByRole('button', { name: 'Update Deficiency' }).click();
-    await expect.soft(page.getByText('No Results to Show')).toBeVisible();
-  
-    await page.getByText('Open').click();
-    await page.getByRole('button', { name: 'Done' }).click();
     await expect.soft(page.getByRole('cell', { name: '16.12.2022' })).toBeVisible();
     await expect.soft(page.getByRole('cell', { name: 'Innenbereich' })).toBeVisible();
     await expect.soft(page.getByRole('cell', { name: 'siehe Hinweistext' })).toBeVisible();
@@ -51,13 +50,6 @@ test('test', async ({ page }) => {
     await expect.soft(page.getByRole('cell', { name: 'NA' })).toBeVisible();
 
     //Reset changed data
-    await page.getByRole('cell', { name: 'Innenbereich' }).click();
-    await page.getByText('test').click();
-    await page.getByText('test').fill('');
-    await page.getByLabel('Aktualisierung von Mängeln').locator('span').first().click();
-    await page.getByRole('button', { name: 'Update Deficiency' }).click();
-    await page.getByText('Done').first().click();
-    await page.getByRole('button', { name: 'Open' }).click();
     await page.getByRole('cell', { name: 'Innenbereich' }).click();
     await expect.soft(page.getByRole('textbox')).toHaveText('');
     await expect.soft(page.getByRole('textbox')).not.toHaveText('test123');
